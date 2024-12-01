5dd7843061b0d05950beb29f0092c7675e6dffb0
Su Yang <soulteary@gmail.com>
2024-12-01 20:47:57 +0800

    chore: refactor rewriter

--- internal/rewriter/rewriter.go
+++ internal/rewriter/rewriter.go
@@ -5,252 +5,179 @@ import (
 	"net/http"
 	"net/url"
 	"regexp"
+	"sync"
 
-	Define "github.com/soulteary/apt-proxy/define"
-	Benchmark "github.com/soulteary/apt-proxy/internal/benchmark"
-	Mirrors "github.com/soulteary/apt-proxy/internal/mirrors"
-	State "github.com/soulteary/apt-proxy/state"
+	"github.com/soulteary/apt-proxy/define"
+	"github.com/soulteary/apt-proxy/internal/benchmarks"
+	"github.com/soulteary/apt-proxy/internal/mirrors"
+	"github.com/soulteary/apt-proxy/state"
 )
 
+// URLRewriter holds the mirror and pattern for URL rewriting
+type URLRewriter struct {
+	mirror  *url.URL
+	pattern *regexp.Regexp
+}
+
+// URLRewriters manages rewriters for different distributions
 type URLRewriters struct {
 	ubuntu      *URLRewriter
 	ubuntuPorts *URLRewriter
 	debian      *URLRewriter
 	centos      *URLRewriter
 	alpine      *URLRewriter
+	mu          sync.RWMutex
 }
 
-type URLRewriter struct {
-	mirror  *url.URL
-	pattern *regexp.Regexp
+// getRewriterConfig returns configuration for a specific distribution
+func getRewriterConfig(mode int) (getMirror func() *url.URL, name string) {
+	switch mode {
+	case define.TYPE_LINUX_DISTROS_UBUNTU:
+		return state.GetUbuntuMirror, "Ubuntu"
+	case define.TYPE_LINUX_DISTROS_UBUNTU_PORTS:
+		return state.GetUbuntuPortsMirror, "Ubuntu Ports"
+	case define.TYPE_LINUX_DISTROS_DEBIAN:
+		return state.GetDebianMirror, "Debian"
+	case define.TYPE_LINUX_DISTROS_CENTOS:
+		return state.GetCentOSMirror, "CentOS"
+	case define.TYPE_LINUX_DISTROS_ALPINE:
+		return state.GetAlpineMirror, "Alpine"
+	default:
+		return nil, ""
+	}
 }
 
-func GetRewriteRulesByMode(mode int) (rules []Define.Rule) {
-	if mode == Define.TYPE_LINUX_DISTROS_UBUNTU {
-		return Define.UBUNTU_DEFAULT_CACHE_RULES
-	}
-	if mode == Define.TYPE_LINUX_DISTROS_UBUNTU_PORTS {
-		return Define.UBUNTU_PORTS_DEFAULT_CACHE_RULES
-	}
-	if mode == Define.TYPE_LINUX_DISTROS_DEBIAN {
-		return Define.DEBIAN_DEFAULT_CACHE_RULES
-	}
-	if mode == Define.TYPE_LINUX_DISTROS_CENTOS {
-		return Define.CENTOS_DEFAULT_CACHE_RULES
-	}
-	if mode == Define.TYPE_LINUX_DISTROS_ALPINE {
-		return Define.ALPINE_DEFAULT_CACHE_RULES
+// createRewriter creates a new URLRewriter for a specific distribution
+func createRewriter(mode int) *URLRewriter {
+	getMirror, name := getRewriterConfig(mode)
+	if getMirror == nil {
+		return nil
 	}
 
-	rules = append(rules, Define.UBUNTU_DEFAULT_CACHE_RULES...)
-	rules = append(rules, Define.UBUNTU_PORTS_DEFAULT_CACHE_RULES...)
-	rules = append(rules, Define.DEBIAN_DEFAULT_CACHE_RULES...)
-	rules = append(rules, Define.CENTOS_DEFAULT_CACHE_RULES...)
-	rules = append(rules, Define.ALPINE_DEFAULT_CACHE_RULES...)
-	return rules
-}
-
-func getRewriterForAlpine() *URLRewriter {
-	u := &URLRewriter{}
-	mirror := State.GetAlpineMirror()
-	benchmarkUrl, pattern := Mirrors.GetPredefinedConfiguration(Define.TYPE_LINUX_DISTROS_ALPINE)
-	u.pattern = pattern
+	benchmarkURL, pattern := mirrors.GetPredefinedConfiguration(mode)
+	rewriter := &URLRewriter{pattern: pattern}
+	mirror := getMirror()
 
 	if mirror != nil {
-		log.Printf("using specify Alpine mirror %s", mirror)
-		u.mirror = mirror
-		return u
+		log.Printf("using specified %s mirror %s", name, mirror)
+		rewriter.mirror = mirror
+		return rewriter
 	}
 
-	mirrors := Mirrors.GetGeoMirrorUrlsByMode(Define.TYPE_LINUX_DISTROS_ALPINE)
-	fastest, err := Benchmark.GetTheFastestMirror(mirrors, benchmarkUrl)
+	mirrorURLs := mirrors.GetGeoMirrorUrlsByMode(mode)
+	fastest, err := benchmarks.GetTheFastestMirror(mirrorURLs, benchmarkURL)
 	if err != nil {
-		log.Println("Error finding fastest Alpine mirror", err)
+		log.Printf("Error finding fastest %s mirror: %v", name, err)
+		return rewriter
 	}
 
 	if mirror, err := url.Parse(fastest); err == nil {
-		log.Printf("using fastest Alpine mirror %s", fastest)
-		u.mirror = mirror
+		log.Printf("using fastest %s mirror %s", name, fastest)
+		rewriter.mirror = mirror
 	}
 
-	return u
+	return rewriter
 }
 
-func getRewriterForCentOS() *URLRewriter {
-	u := &URLRewriter{}
-	mirror := State.GetCentOSMirror()
-	benchmarkUrl, pattern := Mirrors.GetPredefinedConfiguration(Define.TYPE_LINUX_DISTROS_CENTOS)
-	u.pattern = pattern
-
-	if mirror != nil {
-		log.Printf("using specify CentOS mirror %s", mirror)
-		u.mirror = mirror
-		return u
-	}
-
-	mirrors := Mirrors.GetGeoMirrorUrlsByMode(Define.TYPE_LINUX_DISTROS_CENTOS)
-	fastest, err := Benchmark.GetTheFastestMirror(mirrors, benchmarkUrl)
-	if err != nil {
-		log.Println("Error finding fastest CentOS mirror", err)
-	}
-
-	if mirror, err := url.Parse(fastest); err == nil {
-		log.Printf("using fastest CentOS mirror %s", fastest)
-		u.mirror = mirror
-	}
-
-	return u
-}
-
-func getRewriterForDebian() *URLRewriter {
-	u := &URLRewriter{}
-	mirror := State.GetDebianMirror()
-	benchmarkUrl, pattern := Mirrors.GetPredefinedConfiguration(Define.TYPE_LINUX_DISTROS_DEBIAN)
-	u.pattern = pattern
-
-	if mirror != nil {
-		log.Printf("using specify Debian mirror %s", mirror)
-		u.mirror = mirror
-		return u
-	}
-
-	mirrors := Mirrors.GetGeoMirrorUrlsByMode(Define.TYPE_LINUX_DISTROS_DEBIAN)
-	fastest, err := Benchmark.GetTheFastestMirror(mirrors, benchmarkUrl)
-	if err != nil {
-		log.Println("Error finding fastest Debian mirror", err)
-	}
-
-	if mirror, err := url.Parse(fastest); err == nil {
-		log.Printf("using fastest Debian mirror %s", fastest)
-		u.mirror = mirror
-	}
-
-	return u
-}
-
-func getRewriterForUbuntu() *URLRewriter {
-	u := &URLRewriter{}
-	mirror := State.GetUbuntuMirror()
-	benchmarkUrl, pattern := Mirrors.GetPredefinedConfiguration(Define.TYPE_LINUX_DISTROS_UBUNTU)
-	u.pattern = pattern
-
-	if mirror != nil {
-		log.Printf("using specify Ubuntu mirror %s", mirror)
-		u.mirror = mirror
-		return u
-	}
-
-	mirrors := Mirrors.GetGeoMirrorUrlsByMode(Define.TYPE_LINUX_DISTROS_UBUNTU)
-	fastest, err := Benchmark.GetTheFastestMirror(mirrors, benchmarkUrl)
-	if err != nil {
-		log.Println("Error finding fastest Ubuntu mirror", err)
-	}
-
-	if mirror, err := url.Parse(fastest); err == nil {
-		log.Printf("using fastest Ubuntu mirror %s", fastest)
-		u.mirror = mirror
-	}
-
-	return u
-}
-
-func getRewriterForUbuntuPorts() *URLRewriter {
-	u := URLRewriter{}
-	mirror := State.GetUbuntuPortsMirror()
-	benchmarkUrl, pattern := Mirrors.GetPredefinedConfiguration(Define.TYPE_LINUX_DISTROS_UBUNTU_PORTS)
-	u.pattern = pattern
-
-	if mirror != nil {
-		log.Printf("using specify Ubuntu Ports mirror %s", mirror)
-		u.mirror = mirror
-		return &u
-	}
-
-	mirrors := Mirrors.GetGeoMirrorUrlsByMode(Define.TYPE_LINUX_DISTROS_UBUNTU_PORTS)
-	fastest, err := Benchmark.GetTheFastestMirror(mirrors, benchmarkUrl)
-	if err != nil {
-		log.Println("Error finding fastest Ubuntu Ports mirror", err)
-	}
-
-	if mirror, err := url.Parse(fastest); err == nil {
-		log.Printf("using fastest Ubuntu Ports mirror %s", fastest)
-		u.mirror = mirror
-	}
-
-	return &u
-}
-
+// CreateNewRewriters initializes rewriters based on mode
 func CreateNewRewriters(mode int) *URLRewriters {
 	rewriters := &URLRewriters{}
 
-	if mode == Define.TYPE_LINUX_DISTROS_UBUNTU {
-		rewriters.ubuntu = getRewriterForUbuntu()
-		return rewriters
+	switch mode {
+	case define.TYPE_LINUX_DISTROS_UBUNTU:
+		rewriters.ubuntu = createRewriter(mode)
+	case define.TYPE_LINUX_DISTROS_UBUNTU_PORTS:
+		rewriters.ubuntuPorts = createRewriter(mode)
+	case define.TYPE_LINUX_DISTROS_DEBIAN:
+		rewriters.debian = createRewriter(mode)
+	case define.TYPE_LINUX_DISTROS_CENTOS:
+		rewriters.centos = createRewriter(mode)
+	case define.TYPE_LINUX_DISTROS_ALPINE:
+		rewriters.alpine = createRewriter(mode)
+	default:
+		rewriters.ubuntu = createRewriter(define.TYPE_LINUX_DISTROS_UBUNTU)
+		rewriters.ubuntuPorts = createRewriter(define.TYPE_LINUX_DISTROS_UBUNTU_PORTS)
+		rewriters.debian = createRewriter(define.TYPE_LINUX_DISTROS_DEBIAN)
+		rewriters.centos = createRewriter(define.TYPE_LINUX_DISTROS_CENTOS)
+		rewriters.alpine = createRewriter(define.TYPE_LINUX_DISTROS_ALPINE)
 	}
 
-	if mode == Define.TYPE_LINUX_DISTROS_UBUNTU_PORTS {
-		rewriters.ubuntuPorts = getRewriterForUbuntuPorts()
-		return rewriters
-	}
-
-	if mode == Define.TYPE_LINUX_DISTROS_DEBIAN {
-		rewriters.debian = getRewriterForDebian()
-		return rewriters
-	}
-
-	if mode == Define.TYPE_LINUX_DISTROS_CENTOS {
-		rewriters.centos = getRewriterForCentOS()
-		return rewriters
-	}
-
-	if mode == Define.TYPE_LINUX_DISTROS_ALPINE {
-		rewriters.alpine = getRewriterForAlpine()
-		return rewriters
-	}
-
-	rewriters.ubuntu = getRewriterForUbuntu()
-	rewriters.ubuntuPorts = getRewriterForUbuntuPorts()
-	rewriters.debian = getRewriterForDebian()
-	rewriters.centos = getRewriterForCentOS()
-	rewriters.alpine = getRewriterForAlpine()
 	return rewriters
 }
 
-func RewriteRequestByMode(r *http.Request, rewriters *URLRewriters, mode int) {
-	uri := r.URL.String()
-
-	var rewriter *URLRewriter
+// GetRewriteRulesByMode returns caching rules for a specific mode
+func GetRewriteRulesByMode(mode int) []define.Rule {
 	switch mode {
-	case Define.TYPE_LINUX_DISTROS_UBUNTU:
-		rewriter = rewriters.ubuntu
-	case Define.TYPE_LINUX_DISTROS_UBUNTU_PORTS:
-		rewriter = rewriters.ubuntuPorts
-	case Define.TYPE_LINUX_DISTROS_DEBIAN:
-		rewriter = rewriters.debian
-	case Define.TYPE_LINUX_DISTROS_CENTOS:
-		rewriter = rewriters.centos
-	case Define.TYPE_LINUX_DISTROS_ALPINE:
-		rewriter = rewriters.alpine
-	}
-
-	if rewriter.mirror != nil && rewriter.pattern != nil && rewriter.pattern.MatchString(uri) {
-		r.Header.Add("Content-Location", uri)
-		m := rewriter.pattern.FindAllStringSubmatch(uri, -1)
-		// Fix the problem of double escaping of symbols
-		queryRaw := m[0][len(m[0])-1]
-		unescapedQuery, err := url.PathUnescape(queryRaw)
-		if err != nil {
-			unescapedQuery = queryRaw
-		}
-		r.URL.Scheme = rewriter.mirror.Scheme
-		r.URL.Host = rewriter.mirror.Host
-		r.URL.Path = rewriter.mirror.Path + unescapedQuery
+	case define.TYPE_LINUX_DISTROS_UBUNTU:
+		return define.UBUNTU_DEFAULT_CACHE_RULES
+	case define.TYPE_LINUX_DISTROS_UBUNTU_PORTS:
+		return define.UBUNTU_PORTS_DEFAULT_CACHE_RULES
+	case define.TYPE_LINUX_DISTROS_DEBIAN:
+		return define.DEBIAN_DEFAULT_CACHE_RULES
+	case define.TYPE_LINUX_DISTROS_CENTOS:
+		return define.CENTOS_DEFAULT_CACHE_RULES
+	case define.TYPE_LINUX_DISTROS_ALPINE:
+		return define.ALPINE_DEFAULT_CACHE_RULES
+	default:
+		rules := make([]define.Rule, 0)
+		rules = append(rules, define.UBUNTU_DEFAULT_CACHE_RULES...)
+		rules = append(rules, define.UBUNTU_PORTS_DEFAULT_CACHE_RULES...)
+		rules = append(rules, define.DEBIAN_DEFAULT_CACHE_RULES...)
+		rules = append(rules, define.CENTOS_DEFAULT_CACHE_RULES...)
+		rules = append(rules, define.ALPINE_DEFAULT_CACHE_RULES...)
+		return rules
 	}
 }
 
-func MatchingRule(subject string, rules []Define.Rule) (*Define.Rule, bool) {
+// RewriteRequestByMode rewrites the request URL based on the mode
+func RewriteRequestByMode(r *http.Request, rewriters *URLRewriters, mode int) {
+	rewriters.mu.RLock()
+	defer rewriters.mu.RUnlock()
+
+	var rewriter *URLRewriter
+	switch mode {
+	case define.TYPE_LINUX_DISTROS_UBUNTU:
+		rewriter = rewriters.ubuntu
+	case define.TYPE_LINUX_DISTROS_UBUNTU_PORTS:
+		rewriter = rewriters.ubuntuPorts
+	case define.TYPE_LINUX_DISTROS_DEBIAN:
+		rewriter = rewriters.debian
+	case define.TYPE_LINUX_DISTROS_CENTOS:
+		rewriter = rewriters.centos
+	case define.TYPE_LINUX_DISTROS_ALPINE:
+		rewriter = rewriters.alpine
+	}
+
+	if rewriter == nil || rewriter.mirror == nil || rewriter.pattern == nil {
+		return
+	}
+
+	uri := r.URL.String()
+	if !rewriter.pattern.MatchString(uri) {
+		return
+	}
+
+	r.Header.Add("Content-Location", uri)
+	matches := rewriter.pattern.FindStringSubmatch(uri)
+	if len(matches) == 0 {
+		return
+	}
+
+	queryRaw := matches[len(matches)-1]
+	unescapedQuery, err := url.PathUnescape(queryRaw)
+	if err != nil {
+		unescapedQuery = queryRaw
+	}
+
+	r.URL.Scheme = rewriter.mirror.Scheme
+	r.URL.Host = rewriter.mirror.Host
+	r.URL.Path = rewriter.mirror.Path + unescapedQuery
+}
+
+// MatchingRule finds a matching rule for the given path
+func MatchingRule(path string, rules []define.Rule) (*define.Rule, bool) {
 	for _, rule := range rules {
-		if rule.Pattern.MatchString(subject) {
+		if rule.Pattern.MatchString(path) {
 			return &rule, true
 		}
 	}
